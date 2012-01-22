3a166be9c45a3c0b745c117c303e31c2bd42f254
arpruss <arpruss@62f2df5f-5de1-585f-d330-a13794a377f9>
2012-01-22 18:22:18 +0000

    temp fix for omega centauri mixup problem

--- app/src/mobi/omegacentauri/ao/activities/ShowLicense.java
+++ app/src/mobi/omegacentauri/ao/activities/ShowLicense.java
@@ -33,12 +33,24 @@ import mobi.omegacentauri.ao.R;
 public class ShowLicense extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle icicle) {
-    	Resources r = getResources();
         super.onCreate(icicle);
+
+        Resources r = getResources();
+
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle(r.getString(R.string.license_title));
-        alertDialog.setMessage(Html.fromHtml(r.getString(R.string.license)));
-        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, r.getString(android.R.string.ok), 
+        alertDialog.setTitle("Apache License, Version 2.0");
+//        alertDialog.setMessage(Html.fromHtml(r.getString(R.string.license)));
+        alertDialog.setMessage(Html.fromHtml("Copyright 2008-11 Google, Inc. and 2012 Omega Centauri<br/>"+
+        "Licensed under the Apache License, Version 2.0 (the \"License\"); "+
+        "you may not use this file except in compliance with the License. "+
+        "You may obtain a copy of the License at<br/> "+
+        "http://www.apache.org/licenses/LICENSE-2.0.<br/> "+
+        "Unless required by applicable law or agreed to in writing, software "+
+        "distributed under the License is distributed on an \"AS IS\" BASIS, "+
+        "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
+        "See the License for the specific language governing permissions and "+
+        "limitations under the License."));
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, r.getString(android.R.string.ok),
         	new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {finish();} });
         alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
